490012c297994e861e2dec45b12f67861e75e62a
Maciej Sikora <maciejsikora2302@gmail.com>
2020-04-11 11:14:04 +0200

    sender and recever working with mosquitto

---
+++ recever.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("I manage to connect to server")
+    else:
+        print("Something went wrong with connecting, rc = ", rc)
+def on_log(client, userdata, level, buf):
+    print("Log: ", buf)
+
+def on_messege(client, userdata, msg):
+    decoded = msg.payload.decode("utf-8")
+    print(f"Message I received -> Topic: {msg.topic}, Message: {decoded}")
+
+brooker = "127.0.0.1"
+client = mqtt.Client("recever")
+
+client.on_connect = on_connect
+client.on_log = on_log
+client.on_message = on_messege
+
+client.connect(brooker)
+client.subscribe("test_topic")
+client.loop_start()
+time.sleep(100)
+client.loop_stop()
--- sender.py
+++ sender.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import time
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -8,12 +9,19 @@ def on_connect(client, userdata, flags, rc):
 def on_log(client, userdata, level, buf):
     print("Log: ", buf)
 
+def on_messege(client, userdata, msg):
+    decoded = msg.payload.decode("utf-8")
+    print(f"Message I received -> Topic: {msg.topic}, Message: {decoded}")
+
 brooker = "127.0.0.1"
-client = mqtt.Client("test_client")
+client = mqtt.Client("sender")
 
 client.on_connect = on_connect
-client. on_log = on_log
+client.on_log = on_log
+client.on_message = on_messege
 
 client.connect(brooker)
+# client.subscribe("test_topic")
 client.loop_start()
+client.publish("test_topic", "first msg with special signs ;'-=>,;")
 client.loop_stop()
