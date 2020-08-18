06f69b6f0ede9281ae439de5ee297a9b7ae69281
p1003 <pitrus1003@gmail.com>
2020-08-18 14:00:27 +0200

    csv reader
    csv - source of our data
    for now not in database

---
+++ csv_reader.py
@@ -0,0 +1,15 @@
+import csv
+
+def getData(fileName):
+    with open(fileName, newline='') as csvfile:
+        sdtreader = csv.reader(csvfile, delimiter=' ')
+        for row in sdtreader:
+            print(', '.join(row))
+
+with open('2020_synop_csv/s_d_t_01_2020.csv', newline='') as csvfile:
+    fieldNames = ['number','city','year','month','day','cloud','NOS','wind','FWS','temp','TEMP','pressW','CPW','moisture','WLGS','press','PPPS','pressSea','PPPM','rainDay','WODZ','rainNight','WONO']
+    sdtreader = csv.DictReader(csvfile,fieldnames=fieldNames)
+    for row in sdtreader:
+        print(row['number'], row['wind'], row['press'])
+        word = row['press']
+        print(type(word),type(row))
