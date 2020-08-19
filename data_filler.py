027651461c4008592f717ed27e8f79bd83d5c3fb
p1003 <pitrus1003@gmail.com>
2020-08-19 13:36:43 +0200

    detail

--- data_filler.py
+++ data_filler.py
@@ -12,23 +12,23 @@ def fill(filename):
         fieldNames = ['number','city','year','month','day','cloud','NOS','wind','FWS','temp','TEMP','pressW','CPW','moisture','WLGS','press','PPPS','pressSea','PPPM','rainDay','WODZ','rainNight','WONO']
         sdtreader = csv.DictReader(csvfile,fieldnames=fieldNames)
         for row in sdtreader:
-            newDict = dict ([
-                ("date",row['year']+'-'+row['month']+'-'+row['day']),
-                ("hour",8),
-                ("temp",row['temp']),
-                ("wind",float(row['wind'])),
-                ("press",float(row['press'])),
-                ("rain",float(row['rainDay'])+float(row['rainNight']))
-            ])
-            print(newDict)
-            if not db.contains((query.date == newDict.get("date")) & (query.hour == newDict.get("hour"))):
-                db.insert(newDict)
+            if(row['number'] == '350190566'):
+                newDict = dict ([
+                    ("date",row['year']+'-'+row['month']+'-'+row['day']),
+                    ("hour",8),
+                    ("temp",row['temp']),
+                    ("wind",float(row['wind'])),
+                    ("press",float(row['press'])),
+                    ("rain",float(row['rainDay'])+float(row['rainNight']))
+                ])
+                if not db.contains((query.date == newDict.get("date")) & (query.hour == newDict.get("hour"))):
+                    db.insert(newDict)
 
 
 filenames = ['s_d_t_01_2020.csv','s_d_t_02_2020.csv','s_d_t_03_2020.csv','s_d_t_04_2020.csv','s_d_t_05_2020.csv','s_d_t_06_2020.csv','s_d_t_07_2020.csv']
 
-# for filename in filenames:
-#     fill(filename)
+for filename in filenames:
+    fill(filename)
 
 # def insert_test_data():
 #     db.insert({"date": "2020-01-01", "hour": 8, "temp": 9.8, "wind": 5, "press": 1015.8, "rain": 1.4})
